32d26971bbeb103aa08d9990c331642793a13838
Warren Phen <warren.phen@gmail.com>
2016-10-13 22:01:24 -0400

    first commit

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -5,11 +5,7 @@ require('../../scss/style.scss');
 
 const App = () => (
     <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
+        <h3>Photos</h3>
     </div>
 );
 
