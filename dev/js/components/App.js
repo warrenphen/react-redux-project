2f5a6b81482a1e6c015e999df7613b357a525846
Warren Phen <warren.phen@gmail.com>
2016-10-15 14:23:00 -0400

    deleted files

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
 require('../../scss/style.scss');
 
+
 const App = () => (
-    <div>
-        <h3>Photos</h3>
+    <div className="ui container">
+ 
+      <h1>asd</h1>
     </div>
 );
 
