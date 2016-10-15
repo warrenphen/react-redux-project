44f452084339fe4d6fd087cb32b3a4e921472245
Warren Phen <warren.phen@gmail.com>
2016-10-15 15:51:21 -0400

    implementing layout

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import {
+  default as React,
+  Component,
+} from "react";
+
 require('../../scss/style.scss');
 
+import {
+  withGoogleMap,
+  GoogleMap,
+} from "react-google-maps";
 
-const App = () => (
-    <div className="ui container">
- 
-      <h1>asd</h1>
-    </div>
-);
+const SimpleMapExampleGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: 43.7615, lng: -79.383 }}
+  />
+));
 
-export default App;
+export default class App extends Component {
+	render() {
+    return (
+    	<div className="container">
+	    <div className="ui two column">
+
+	      <div className="ui sidebar visible vertical menu" style={{position: 'relative', width: '20%', height: '100%', float: 'left'}}>
+		    <a className="item">
+		      Restaurants
+		    </a>
+		    <a className="item">
+		      Gas Stations
+		    </a>
+		    <a className="item">
+		      Coffee Shops
+		    </a>
+		  </div>
+
+		  <div style={{backgroundColor: 'black', width: '80%', height: '100%', float: 'left'}}> 
+			<SimpleMapExampleGoogleMap
+	        containerElement={
+	          <div style={{ height: '100%', width: '100%' }} />
+	        }
+	        mapElement={
+	          <div style={{ height: '700px', width: '100%'}} />
+	        }
+	      />
+	      </div>
+
+	      </div>
+	      </div>
+		);
+	}
+}
