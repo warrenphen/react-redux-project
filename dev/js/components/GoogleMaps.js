cd7dccbff4756e51f8faccd42a859587cd8abd1a
Warren Phen <warren.phen@gmail.com>
2016-10-17 15:23:28 -0400

    pulling establishments from google api working

--- dev/js/components/GoogleMaps.js
+++ dev/js/components/GoogleMaps.js
@@ -16,6 +16,21 @@ import {
 
 const TORONTO = {lat: 43.7615, lng: -79.383};
 
+const INPUT_STYLE = {
+  boxSizing: `border-box`,
+  MozBoxSizing: `border-box`,
+  border: `1px solid transparent`,
+  width: `240px`,
+  height: `32px`,
+  marginTop: `27px`,
+  padding: `0 12px`,
+  borderRadius: `1px`,
+  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+  fontSize: `14px`,
+  outline: `none`,
+  textOverflow: `ellipses`,
+};
+
 const Maps = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={10}
@@ -33,7 +48,7 @@ const Maps = withGoogleMap(props => (
 export default class GoogleMaps extends Component {
 	
 	constructor(props) {
-	super(props);
+		super(props);
 
 	  this.state = {
 	    markers: [{
@@ -53,6 +68,43 @@ export default class GoogleMaps extends Component {
 	      defaultAnimation: 2,
 	    }],
 	  };
+	}
+
+	componentDidMount() {
+
+		const toronto = new google.maps.LatLng(43.7515, -79.373);
+
+		const map = new google.maps.Map({
+      center: TORONTO,
+      zoom: 15
+    });
+
+ 		const request = {
+	    location: toronto,
+	    radius: '2000',
+    	type: ['gym']
+  	};
+ 
+		const service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, this.callback.bind(this));
+	}
+
+	callback(results, status) {
+		const markers = [];
+
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+      alert(status);
+      return;
+    }
+    for (let result of results) {
+    	console.log(result);
+    	const marker = {
+				position: result.geometry.location
+			}
+      markers.push(marker);
+    }
+ 
+    this.setState({markers: markers});
 	}
 
 	render() {
